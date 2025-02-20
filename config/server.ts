28353de13bbfb339a345cc384fe7630a0e7d5861
Ivan Cheng <ivan.cheng011099@gmail.com>
2025-02-19 21:37:15 -0800

    hello

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env('PORT', 1337),
+  url: env('MY_HEROKU_URL'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['key1', 'key2', 'key3', 'key4']),
   },
 });
